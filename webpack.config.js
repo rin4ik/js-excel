33293a207d6658bc20f8ee3b8b60a7183f481326
rin4ik <m.mirakhmad@gmail.com>
2021-07-21 13:06:57 +0500

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,55 @@
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports =  {
      context: path.resolve(__dirname, 'src'),
      mode: 'development',
      entry: './index.js',
      output: {
-         filename: 'bundle.js',
+         filename: 'bundle.[hash].js',
          path: path.resolve(__dirname, 'dist')
-     }
+     },
+     resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        }
+     },
+     
+     module: {
+        rules: [
+            {
+                test: /\.s[acc]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            { 
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ],
+     },
+     plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            'filename': 'bundle.[hash].css'
+        })
+     ]
  }
