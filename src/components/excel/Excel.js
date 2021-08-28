430d43439582ab6167b59759c2fa7fcd3c50676b
rin4ik <m.mirakhmad@gmail.com>
2021-08-28 17:06:43 +0500

    js structure WIP

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,18 @@ export class Excel {
         this.$el = document.querySelector(selector)
         this.components = options.components || []
     }
+    getRoot() {
+        // afterbegin, afterend, befoeend, beforebegin
+        // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
+        const $root = document.createElement('div')
+        $root.className = 'excel'
+        this.components.forEach(Component => {
+            const component = new Component()
+            $root.insertAdjacentHTML('beforeend', component.toHTML())
+        })
+        return $root
+    }
+    render() {
+        this.$el.append(this.getRoot());
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,80 +8,80 @@ const isDev = !isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 const jsLoaders = () => {
-  const loaders = [{
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env']
+    const loaders = [{
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env']
+        }
+    }]
+    if (isDev) {
+        loaders.push('eslint-loader')
     }
-  }]
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
-  return loaders
+    return loaders
 }
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: ['@babel/polyfill', './index.js'],
-  output: {
-    filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
-    }
-  },
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './index.js'],
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        }
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
             }
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: jsLoaders()
-      }
+        ]
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 9000,
+        hot: true,
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html',
+            title: 'Hot Module Replacement',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyPlugin([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]),
+        new MiniCssExtractPlugin({
+            'filename': filename('css')
+        })
     ]
-  },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
-    port: 9000,
-    hot: true,
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      template: 'index.html',
-      title: 'Hot Module Replacement',
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd
-      }
-    }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
-    new MiniCssExtractPlugin({
-      'filename': filename('css')
-    })
-  ]
 }
