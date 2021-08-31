1d84df360cecb17e517862fed8b9045237c82822
rin4ik <m.mirakhmad@gmail.com>
2021-08-31 12:11:59 +0500

    dom interactions

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,10 +1,35 @@
 class Dom {
-    constructor() {
+    constructor(selector) {
+        this.$el =typeof selector === 'string'
+            ? document.querySelector(selector)
+            : selector
+    }
+    html(html) {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this;
+        }
+        return this.$el.outerHTML.trim()
+    }
+    clear() {
+        this.html('')
+        return this
+    }
 
+    append(node) {
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+        return this
     }
 }
-export function $() {
-    return new Dom();
+export function $(selector) {
+    return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
@@ -12,5 +37,5 @@ $.create = (tagName, classes = '') => {
     if (classes) {
         el.classList.add(classes)
     }
-    return el
+    return $(el)
 }
