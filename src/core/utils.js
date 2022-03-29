bb8fe242e3982ed6953dd1da1be9942f952a2152
rin4ik <m.mirakhmad@gmail.com>
2022-03-29 14:11:37 +0500

    isEqual function

--- src/core/utils.js
+++ src/core/utils.js
@@ -19,3 +19,9 @@ export function storage(key, data) {
     }
     localStorage.setItem(key, JSON.stringify(data))
 }
+export function isEqual(a, b) {
+    if (typeof a === 'object' && typeof b === 'object') {
+        return JSON.stringify(a) === JSON.stringify(b)
+    }
+    return a === b
+}
