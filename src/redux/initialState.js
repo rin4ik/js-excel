d99a6fada3c7b941b76279d42668132db7046b99
rin4ik <m.mirakhmad@gmail.com>
2022-04-02 12:43:48 +0500

    normalize

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -5,9 +5,15 @@ const defaultState = {
     rowState: {},
     colState: {},
     dataState: {},
+    stylesState: {},
     currentStyle: defaultStyles,
     currentText: '',
 }
+const normalize = state => ({
+    ...state,
+    currentStyles: defaultStyles,
+    currentText: ''
+})
 export const initialState = storage('excel-state')
-    ? storage('excel-state')
+    ? normalize(storage('excel-state'))
     : defaultState
