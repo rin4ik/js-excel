2a1c31fa14cedae7f2f1d2ebd87d6934a397a93e
rin4ik <m.mirakhmad@gmail.com>
2022-04-04 10:53:56 +0500

    creating main page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,38 +1,23 @@
 import { Page } from '../core/Page';
 import {$} from '@core/dom'
+import { createRecordsTable } from './dashboard.functions';
 
 export class DashboardPage extends Page {
     getRoot() {
+        const now = Date.now().toString()
         return $.create('div', 'db').html(`
             <div class="db__header">
                 <h1>Excel Dashboard</h1>
             </div>
             <div class="db__new">
                 <div class="db__view">
-                <a href="#" class="db__create">
+                <a href="#excel/${now}" class="db__create">
                     New <br> Table
                 </a>
                 </div>
             </div>
             <div class="db__table db__view">
-            <div class="db__list-header">
-                <span>Name</span>
-                <span>Creation date</span>
-            </div>
-            <ul class="db__list">
-                <li class="db__record"> 
-                    <a href="#">Table Number</a>
-                    <strong>27.08.2021</strong>
-                </li>
-                <li class="db__record"> 
-                    <a href="#">Table Number</a>
-                    <strong>27.08.2021</strong>
-                </li>
-                <li class="db__record"> 
-                    <a href="#">Table Number</a>
-                    <strong>27.08.2021</strong>
-                </li>
-            </ul>
+                ${ createRecordsTable() }
             </div> 
         `)
     }
---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,31 @@
+export function toHTML() {
+    return `
+        <li class="db__record"> 
+            <a href="#">Table Number 1</a>
+            <strong>27.08.2021</strong>
+        </li>
+    `
+}
+function getAllKeys() {
+    const keys = []
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+        if (!key.includes('excel')) continue;
+        keys.push(key)
+    }
+    return keys
+}
+export function createRecordsTable() {
+    const keys = getAllKeys()
+    if (!keys.length) {
+        return `<p>You dont have any records yet!</p>`
+    }
+    return `<div class="db__list-header">
+            <span>Name</span>
+            <span>Creation date</span>
+        </div>
+        <ul class="db__list">
+            ${keys.map(toHTML).join('')}
+        </ul>
+    `
+}
